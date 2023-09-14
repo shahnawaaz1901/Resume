092fef76af9ac000dd8b4d48c67dbb6fd03ce057
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-14 12:09:06 +0530

    Create Bar For Responsiveness

--- script.js
+++
@@ -1,7 +0,0 @@
-let id = setInterval(()=>{
-    if(count == song[index].duration){
-        clearInterval(id);
-    }
-    document.getElementById('TotalTime').innerText = count;
-    count++;
-},1000);
